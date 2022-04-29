93410c5e205bbe4f834312a2bd11663d6bec6a40
amartya-rs <saha.amartya@outlook.com>
2022-04-29 15:56:46 +0530

    refactor: footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import { HeartIcon } from "../../assets/icons";
+import "./footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="h6 font-medium mt-6">
+            Made with <HeartIcon />
+            by amartyars
+        </footer>
+    );
+};
+
+export { Footer };
