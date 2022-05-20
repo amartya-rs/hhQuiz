d39e18638a21d08854cc7e071bde2f594cb283c7
amartya-rs <saha.amartya@outlook.com>
2022-05-20 09:56:40 +0530

    feat: added try other quiz button

--- src/pages/result-page/ResultPage.jsx
+++ src/pages/result-page/ResultPage.jsx
@@ -1,11 +1,13 @@
 import "./result-page.css";
 import { useData } from "../../context/data-context";
 import { Loader } from "../../utils/Loader";
+import { useNavigate } from "react-router-dom";
 
 const ResultPage = () => {
    const {
       state: { quizData, currentCategory, userAnswer, isLoading },
    } = useData();
+   const navigate = useNavigate();
 
    const questionID = quizData && Object.keys(quizData?.[currentCategory].data);
 
@@ -52,29 +54,37 @@ const ResultPage = () => {
                      quizData && calculateScore()
                   }`}</h3>
                </section>
-               {questionID &&
-                  questionID?.map((ele, index) => (
-                     <section key={index} className="quiz-details">
-                        <h5>
-                           {`${ele.toUpperCase()}. ${
-                              quizData?.[currentCategory]?.data?.[ele]?.text
-                           }`}
-                        </h5>
-                        <div className="quiz-options">
-                           {quizData?.[currentCategory]?.data?.[
-                              ele
-                           ]?.options.map((ele, index) => (
-                              <button
-                                 value={ele}
-                                 key={index}
-                                 className={`button ${resultOption(ele)}`}
-                              >
-                                 {ele}
-                              </button>
-                           ))}
-                        </div>
-                     </section>
-                  ))}
+               <>
+                  {questionID &&
+                     questionID?.map((ele, index) => (
+                        <section key={index} className="quiz-details">
+                           <h5>
+                              {`${ele.toUpperCase()}. ${
+                                 quizData?.[currentCategory]?.data?.[ele]?.text
+                              }`}
+                           </h5>
+                           <div className="quiz-options">
+                              {quizData?.[currentCategory]?.data?.[
+                                 ele
+                              ]?.options.map((ele, index) => (
+                                 <button
+                                    value={ele}
+                                    key={index}
+                                    className={`button ${resultOption(ele)}`}
+                                 >
+                                    {ele}
+                                 </button>
+                              ))}
+                           </div>
+                        </section>
+                     ))}
+                  <button
+                     className="button primary mt-4"
+                     onClick={() => navigate("/")}
+                  >
+                     TRY OTHER QUIZZES
+                  </button>
+               </>
             </main>
          )}
       </>
