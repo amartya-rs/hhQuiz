f780ab66cae9ded948e912c5477cb752c63eeaea
amartya-rs <saha.amartya@outlook.com>
2022-05-22 22:08:12 +0530

    feat: signup page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export { QuizPage } from "./quiz-page/QuizPage";
 export { ResultPage } from "./result-page/ResultPage";
 export { Page404 } from "./page-404/Page404";
 export { CategoryQuizPage } from "./category-quiz-page/CategoryQuizPage";
+export { LoginPage } from "./login-page/LoginPage";
+export { SignUpPage } from "./signup-page/SignUpPage";
