976f63dd5fb974f6d560b1162996e2ce92acc7cc
amartya-rs <saha.amartya@outlook.com>
2022-05-22 22:05:13 +0530

    refactor: loader props

--- src/utils/Loader.jsx
+++ src/utils/Loader.jsx
@@ -1,14 +1,14 @@
 import PuffLoader from "react-spinners/PuffLoader";
 import { useTheme } from "../context/theme-context";
 
-const Loader = ({ loading }) => {
+const Loader = ({ loading, size }) => {
    const { theme } = useTheme();
    let isThemeDark = theme === "dark" ? true : false;
 
    return (
       <PuffLoader
          loading={loading}
-         size="4.8rem"
+         size={size ?? "4.8rem"}
          color={isThemeDark ? "rgb(250,250,250)" : "rgb(145, 55, 135)"}
       />
    );
