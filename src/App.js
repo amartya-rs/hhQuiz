ad01595b3ee6deb4382e8af73d8ddeba0d89e161
amartya-rs <saha.amartya@outlook.com>
2022-05-22 22:10:50 +0530

    refactor: added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Footer, TopNav } from "./components";
 import { useTheme } from "./context/theme-context";
+import { Footer, TopNav, RestrictedRoute, PrivateRoute } from "./components";
 import {
    HomePage,
    RulesPage,
@@ -9,6 +9,8 @@ import {
    ResultPage,
    Page404,
    CategoryQuizPage,
+   LoginPage,
+   SignUpPage,
 } from "./pages/index";
 
 function App() {
@@ -18,12 +20,21 @@ function App() {
       <div className={`App ${theme === "dark" ? "dark-theme" : ""}`}>
          <TopNav />
          <Routes>
+            {/*public routes*/}
             <Route path="/" element={<HomePage />} />
             <Route path="/:category" element={<CategoryQuizPage />} />
-            <Route path="/rules" element={<RulesPage />} />
-            <Route path="/:category/quiz" element={<QuizPage />} />
-            <Route path="/result" element={<ResultPage />} />
             <Route path="*" element={<Page404 />} />
+            {/*restricted routes*/}
+            <Route element={<RestrictedRoute />}>
+               <Route path="/login" element={<LoginPage />} />
+               <Route path="/signup" element={<SignUpPage />} />
+            </Route>
+            {/*private routes*/}
+            <Route element={<PrivateRoute />}>
+               <Route path="/rules" element={<RulesPage />} />
+               <Route path="/:category/quiz" element={<QuizPage />} />
+               <Route path="/result" element={<ResultPage />} />
+            </Route>
          </Routes>
          <Footer />
       </div>
